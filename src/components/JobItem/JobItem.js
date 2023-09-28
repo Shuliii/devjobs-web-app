582cbab9ec89022562b23c99d0b42ec5c477573d
Shuliii <ricoharsono@gmail.com>
2023-09-28 22:47:27 +0800

    edit some things

--- src/components/JobItem/JobItem.js
+++ src/components/JobItem/JobItem.js
@@ -1,11 +1,24 @@
 import styles from "./JobItem.module.css";
+
 import blogr from "../../assets/logos/blogr.svg";
+import coffeeroasters from "../../assets/logos/coffeeroasters.svg";
+import creative from "../../assets/logos/creative.svg";
+import crowdfund from "../../assets/logos/crowdfund.svg";
+import maker from "../../assets/logos/maker.svg";
+import mastercraft from "../../assets/logos/mastercraft.svg";
+import officelite from "../../assets/logos/officelite.svg";
+import pod from "../../assets/logos/pod.svg";
+import pomodoro from "../../assets/logos/pomodoro.svg";
+import scoot from "../../assets/logos/scoot.svg";
+import typemaster from "../../assets/logos/typemaster.svg";
+import vector from "../../assets/logos/vector.svg";
 
 const JobItem = (props) => {
-  console.log(props.data);
+  const imgLogo = props.data.logo;
+  console.log(imgLogo);
   return (
     <div className={styles["job-item"]}>
-      <img src="../../assets/logos/blogr.svg" />
+      <img src={props.imgLogo} alt={props.data.company} />
       <div className={styles["job-item-text"]}>
         <div className={styles["job-item-text-1"]}>
           <p>{props.data.postedAt}</p>
